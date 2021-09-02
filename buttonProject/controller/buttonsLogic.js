fcf7c60b46fc0fd00b90470099b2cbe738820c9a
Mex777 <fologeaalexandru@gmail.com>
2021-09-02 23:39:09 +0300

    Refactored code

--- buttonProject/controller/buttonsLogic.js
+++ buttonProject/controller/buttonsLogic.js
@@ -1,65 +1,96 @@
-var maxPoints = 0;
-var points = 0;
-var hearts = 3;
-var buttonsCounter = 0;
-var winnerButton = Math.floor(Math.random() * buttonsCounter) + 1;
+/* eslint-disable no-unused-vars */
+let maxPoints = 0;
+let points = 0;
+let hearts = 3;
+let buttonsCounter = 0;
+let winnerButton = Math.floor(Math.random() * buttonsCounter) + 1;
 
+/**
+ * Checks whether the pressed button is winner or loser
+ * @param {number} ID of the button
+ * @return {false}
+*/
 function checkCorrectButton(ID) {
-    if (ID == winnerButton) {
-        ++points;
-        $("#pointsCounter").text("Points: " + points);
-        alert("CORRECT BUTTON");
-    } else {
-        alert("WRONG BUTTON");
-        deleteHeart();
-    }
+  if (ID == winnerButton) {
+    ++points;
+    $('#pointsCounter').text('Points: ' + points);
+    alert('CORRECT BUTTON');
+  } else {
+    alert('WRONG BUTTON');
+    deleteHeart();
+  }
 
-    winnerButton = Math.floor(Math.random() * buttonsCounter) + 1;
-    return false;
+  winnerButton = Math.floor(Math.random() * buttonsCounter) + 1;
+  return false;
 }
 
+/**
+ * Changes the number of buttons from a previous state to a new one.
+ * @return {false}
+ */
 function changeNumberOfButtons() {
-    let before = Number(buttonsCounter);
-    buttonsCounter = Number(prompt("Number of buttons\n(choose a number between 2 and 99)", 3));
-    while (!(buttonsCounter > 1 && buttonsCounter < 100)) {
-        alert("Invalid input");
-        buttonsCounter = Number(prompt("Number of buttons\n(choose a number between 2 and 99)", 3));
+  const before = Number(buttonsCounter);
+  buttonsCounter = Number(
+      prompt('Number of buttons\n(choose a number between 2 and 99)', 3),
+  );
+
+  while (!(buttonsCounter > 1 && buttonsCounter < 100)) {
+    alert('Invalid input');
+    buttonsCounter = Number(
+        prompt('Number of buttons\n(choose a number between 2 and 99)', 3),
+    );
+  }
+
+  if (buttonsCounter > before) {
+    for (let i = before + 1; i <= buttonsCounter; ++i) {
+      $('#gameButtons').append(
+          '<button type = "button"' +
+              'class = "btn btn-outline-success btn-lg"' + 'id =' +
+                  i + 'onclick = "return checkCorrectButton(id)"> ' +
+                        i +
+          '</button>',
+      );
     }
+  } else if (buttonsCounter < before) {
+    for (let i = before; i >= buttonsCounter + 1; --i) {
+      $('#' + i).remove();
+    }
+  }
+  winnerButton = Math.floor(Math.random() * buttonsCounter) + 1;
 
-    if (buttonsCounter > before)
-        for (var i = before + 1; i <= buttonsCounter; ++i) 
-            $("#gameButtons").append('<button type = "button" class = "btn btn-outline-success btn-lg" id = "' + i + '" onclick = "return checkCorrectButton(id)"> ' + i + ' </button>');
-    else if (buttonsCounter < before)
-        for (var i = before; i >= buttonsCounter + 1; --i) 
-            $("#" + i).remove();
-    winnerButton = Math.floor(Math.random() * buttonsCounter) + 1;    
-
-    return false;
+  return false;
 }
 
+/**
+ * Erases a heart from the screen
+ */
 function deleteHeart() {
-    --hearts;
-    $("#heart").remove();
+  --hearts;
+  $('#heart').remove();
 
-    if (!hearts) {
-        alert("Game Over");
-        restartGame();
-    }
+  if (!hearts) {
+    alert('Game Over');
+    restartGame();
+  }
 }
 
+/**
+* Updated the high score, the points and the hearts.
+*/
 function restartGame() {
-    //updating the high score
-    if (maxPoints <= points) {
-        maxPoints = points;
-        $("#highScore").text("Your high score is: " + maxPoints);
-    }
-                
-    //resetting the points
-    points = 0;
-    $("#pointsCounter").text("Points: " + points);
+  if (maxPoints <= points) {
+    maxPoints = points;
+    $('#highScore').text('Your high score is: ' + maxPoints);
+  }
 
-    //resetting the hearts
-    hearts = 3;    
-    for (let i = 1; i <= hearts; ++i)
-        $("#hp").append('<img src = "../view/heart.png" width = "100" height = "100" id = "heart">');
+  points = 0;
+  $('#pointsCounter').text('Points: ' + points);
+
+  hearts = 3;
+  for (let i = 1; i <= hearts; ++i) {
+    $('#hp').append(
+        '<img src = "../view/heart.png"' +
+            'width = "100" height = "100" id = "heart">',
+    );
+  }
 }
--- dictionaryProject/controller/dictionary.js
+++ dictionaryProject/controller/dictionary.js
@@ -1,30 +1,39 @@
-var dictionary = new Set();
+/* eslint-disable no-unused-vars */
+const dictionary = new Set();
 
+/**
+ * Adds a word in the dictionary
+ * @return {false} when input is empty.
+ */
 function addWord() {
-    let word = document.getElementById("word").value 
-    if (word == "") {
-        alert("input is empty");
-        return false;
-    }
+  const currWord = document.getElementById('word').value;
+  if (currWord == '') {
+    alert('input is empty');
+    return false;
+  }
 
-    if (dictionary.has(word) == false) {
-        dictionary.add(document.getElementById("word").value); 
-        $("#wordList").append('<li> ' + word + ' </li>');
-    } else 
-        alert("Already in list");
+  if (dictionary.has(currWord) == false) {
+    dictionary.add(document.getElementById('word').value);
+    $('#wordList').append('<li> ' + word + ' </li>');
+  } else {
+    alert('Already in list');
+  }
 }
 
+/**
+ * Checks whether the current word is in the dictionary
+ * @return {false}
+ */
 function searchWord() {
-    let word = document.getElementById("toBeSearched").value;
-    if (word == "") {
-        alert("input is empty");
-        return false;
-    }
-
-    if (dictionary.has(word))
-        alert("In list");
-    else
-        alert("Not in list");
-
+  const currWord = document.getElementById('toBeSearched').value;
+  if (currWord == '') {
+    alert('input is empty');
     return false;
+  }
+
+  if (dictionary.has(currWord)) {
+    alert('In list');
+  } else {
+    alert('Not in list');
+  }
 }

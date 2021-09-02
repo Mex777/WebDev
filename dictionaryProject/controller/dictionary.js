b225ac6a4e900dfdd5fdfcddba92bb3b1f7f6cac
Mex777 <fologeaalexandru@gmail.com>
2021-09-02 17:54:02 +0300

    solved a bug

--- dictionaryProject/controller/dictionary.js
+++ dictionaryProject/controller/dictionary.js
@@ -1,4 +1,4 @@
-var dictionary = [];
+var dictionary = new Set();
 
 function addWord() {
     let word = document.getElementById("word").value 
@@ -7,20 +7,22 @@ function addWord() {
         return false;
     }
 
-    dictionary.push(document.getElementById("word").value);
-    $("#wordList").append('<li> ' + word + ' </li>');
+    if (dictionary.size == 0 || dictionary.has(word) == false) {
+        dictionary.add(document.getElementById("word").value); 
+        $("#wordList").append('<li> ' + word + ' </li>');
+    } else 
+        alert("Already in list");
 }
 
 function searchWord() {
     let word = document.getElementById("toBeSearched").value;
     let found = false;
+    if (word == "") {
+        alert("input is empty");
+        return false;
+    }
 
-    dictionary.forEach(function(item) {
-        if (item == word)
-            found = true;
-    })
-
-    if (found)
+    if (dictionary.has(word))
         alert("In list");
     else
         alert("Not in list");

6f6c506bc52e33d6c5ac5a82cd359767eb6d9dc0
Mex777 <fologeaalexandru@gmail.com>
2021-09-04 23:49:47 +0300

    Put the images in a separate folder and cleaned the code a bit

--- hangmanProject/controller/createHangman.js
+++ hangmanProject/controller/createHangman.js
@@ -9,19 +9,15 @@ let wrongGuesses = 0;
  * @return {false}
  */
 function createGame() {
-  wordToGuess = $('#wordToSearch').val();
-
-  // creates the hang
-  // $('#hanger').append('<img>');
-
   // adding the letters in a set
+  wordToGuess = $('#wordToSearch').val();
   for (let i = 0; i < wordToGuess.length; ++i) {
     wantedLetters.add(wordToGuess[i]);
   }
 
   $('#hangmanWordInput').remove();
   $('#guessInput').css('opacity', '1.0');
-  $('#hangMan').append('<img src="state0.png" id="state">');
+  $('#hangMan').append('<img src="hangmanStates/state0.png" id="state">');
 
   return false;
 }
@@ -32,8 +28,9 @@ function createGame() {
  */
 function makeGuess() {
   const guess = $('#guess').val();
+  $('#guess').val('');
 
-  // checks whether the guess from the user is a letter.
+  // checks whether the guess from the user is a letter
   if (!isLetter(guess)) {
     $('#outcome').text('Not letter. Try again!');
     return false;
@@ -46,20 +43,18 @@ function makeGuess() {
 
     // ends game if the user guessed all the letters
     if (wantedLetters.size == foundLetters.size) {
-      $('#outcome').text('Game Over!\nYou won!');
-      $('#guessBar').remove();
+      endGame(1);
     }
   } else {
     ++wrongGuesses;
 
     // changes the state of the hangman and updates the outcome on the page
-    $('#hangMan').html('<img src="state' + wrongGuesses + '.png">')
+    $('#hangMan').html('<img src="hangmanStates/state'+ wrongGuesses +'.png">')
     $('#outcome').text('Wrong guess');
 
-    // ends game when the hangmen is dead.
+    // ends game when the man is dead
     if (wrongGuesses == 6) {
-      $('#outcome').text('Game Over!\nYou lost!');
-      $('#guessBar').remove();
+      endGame(0);
     }
   }
 
@@ -67,13 +62,30 @@ function makeGuess() {
 }
 
 /**
- * Checkes if the parameter is a letter from the alphabet.
+ * Checks if the parameter is a letter from the alphabet
  * @param {string} inp
- * @return {bool} true if it's letter, false otherwise
+ * @return {boolean} true if it's letter, false otherwise
  */
 function isLetter(inp) {
   if (inp.length != 1) {
     return false;
   }
   return (inp >= 'a' && inp <= 'z') || (inp >= 'A' && inp <= 'Z');
+}
+
+/**
+ * Ends the game
+ * @param {int} won 1 if game is won, 0 otherwise
+ */
+function endGame(won) {
+  if (won) {
+    $('#outcome').text('Game Over!\nYou won!');
+    $('#outcome').css('color', 'green');
+  } else {
+    $('#outcome').text('Game Over!\nYou lost!');
+    $('#outcome').css('color', 'red');
+  }
+
+  $('#outcome').css('text-align', 'center');
+  $('#guessBar').remove();
 }
